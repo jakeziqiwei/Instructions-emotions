357bb8c33779fbcf3d4ef21e6c2e369a7397d166
Jake <jakeziqiwei@gmail.com>
2022-10-24 14:57:55 -0500

    Instruction App

--- Instructions/GameViewController.swift
+++ Instructions/GameViewController.swift
@@ -8,27 +8,57 @@
 import UIKit
 import SpriteKit
 import GameplayKit
+import AVFoundation
+import AudioToolbox
 
 class GameViewController: UIViewController {
 
+    @IBAction func Instruction1Action(_ sender: Any) {
+        let frequency = [1, 2, 3, 4, 5, 6]
+        for num in frequency{
+            for _ in 0...(num-1){
+                AudioServicesPlaySystemSound(SystemSoundID(4095))
+                print(num)
+                Thread.sleep(forTimeInterval: 1)
+            
+            }
+            Thread.sleep(forTimeInterval: 4)
+    }
+}
+    @IBAction func instruction2Action(_ sender: Any) {
+        let frequency = [1,2,3,4,5,6]
+        for i in frequency{
+            if i == 1 {
+                AudioServicesPlaySystemSound(SystemSoundID(1311))
+            }
+            if i == 2 {
+                AudioServicesPlaySystemSound(SystemSoundID(1311))
+                Thread.sleep(forTimeInterval: 1)
+                AudioServicesPlaySystemSound(SystemSoundID(1311))
+            }
+            if i == 3 {
+                AudioServicesPlaySystemSound(SystemSoundID(1050))
+            }
+            if i == 4 {
+                AudioServicesPlaySystemSound(SystemSoundID(1050))
+                Thread.sleep(forTimeInterval: 1)
+                AudioServicesPlaySystemSound(SystemSoundID(1050))
+            }
+            if i == 5 {
+                AudioServicesPlaySystemSound(SystemSoundID(1521))
+            }
+            if i == 6 {
+                AudioServicesPlaySystemSound(SystemSoundID(1521))
+                Thread.sleep(forTimeInterval: 1)
+                AudioServicesPlaySystemSound(SystemSoundID(1521))
+            }
+            Thread.sleep(forTimeInterval: 4)
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
